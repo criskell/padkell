a64ac2a84663b0d9dc5032890efbe01dc5b8738a
criskell <cris.moraes.ds@gmail.com>
2025-09-01 04:21:02 -0300

    feat: complete home page to view a paste

--- frontend/app/[paste]/page.tsx
+++ frontend/app/[paste]/page.tsx
@@ -6,44 +6,88 @@ import {
   CalendarIcon,
   CircleUserRoundIcon,
   EyeIcon,
+  InfoIcon,
   MailIcon,
+  MessageSquareTextIcon,
+  StarIcon,
+  ThumbsDownIcon,
+  ThumbsUpIcon,
+  TimerIcon,
 } from 'lucide-react';
 import Editor from '@monaco-editor/react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import Link from 'next/link';
 
 export default function PastePage() {
   return (
     <main className="grow flex flex-col gap-2 max-w-full">
       <div className="flex gap-2 flex-wrap text-zinc-950">
-        <div className="bg-zinc-800 rounded-lg" />
+        <div className="bg-zinc-100 rounded-lg size-10" />
 
         <div className="flex flex-col gap-1">
           <p>Instant Profit Method</p>
 
           <div className="flex gap-3 items-center text-zinc-600 text-xs">
-            <span className="inline-flex gap-1">
+            <span className="inline-flex gap-1 items-center">
               <CircleUserRoundIcon size={14} />
               <span>ASHURA</span>
               <MailIcon size={18} />
             </span>
 
-            <span className="inline-flex gap-1">
+            <span className="inline-flex gap-1 items-center">
               <CalendarIcon size={14} />
               <span>AUG 30TH, 2025</span>
             </span>
 
-            <span className="inline-flex gap-1">
+            <span className="inline-flex gap-1 items-center">
               <EyeIcon size={14} />
               <span>10</span>
+            </span>
+
+            <span className="inline-flex gap-1 items-center">
+              <StarIcon size={14} />
+              <span>0</span>
+            </span>
+
+            <span className="inline-flex gap-1 items-center">
+              <TimerIcon size={14} />
+              <span>60 MIN</span>
+            </span>
+
+            <span className="inline-flex gap-1 items-center">
+              <MessageSquareTextIcon size={14} />
+              <span>add comment</span>
             </span>
           </div>
         </div>
       </div>
 
       <div>
-        <div className="flex items-center text-zinc-700 bg-zinc-100 px-6 py-4 gap-2 rounded-t-lg">
+        <div className="flex items-center text-zinc-700 bg-zinc-100 px-6 py-4 gap-2 rounded-t-lg text-xs">
           <Badge variant="outline">JavaScript</Badge>
-          <span className="text-xs">0.32KB</span>
+          <span>0.32KB</span>
+          <span>|</span>
+          <span>None</span>
+          <span>|</span>
+
+          <Badge variant="outline">
+            <ThumbsUpIcon size={20} />
+          </Badge>
+
+          <Badge variant="outline">
+            <ThumbsDownIcon size={20} />
+          </Badge>
+
+          <Badge variant="outline" className="ml-auto">
+            raw
+          </Badge>
+          <Badge variant="outline">download</Badge>
+          <Badge variant="outline">clone</Badge>
+          <Badge variant="outline">embed</Badge>
+          <Badge variant="outline">print</Badge>
+          <Badge variant="outline">report</Badge>
         </div>
+
         <div className="border border-zinc-100 rounded-lg rounded-t-none p-1">
           <Editor
             height="50vh"
@@ -58,6 +102,21 @@ export default function PastePage() {
             }}
           />
         </div>
+      </div>
+
+      <div className="space-y-2 mt-2">
+        <h2 className="text-md">Add comment</h2>
+
+        <Alert>
+          <InfoIcon />
+          <AlertDescription className="inline-flex">
+            Please,{' '}
+            <Link href="/auth/sign-in" className="text-blue-900">
+              Sign In
+            </Link>
+            to add comment
+          </AlertDescription>
+        </Alert>
       </div>
     </main>
   );
